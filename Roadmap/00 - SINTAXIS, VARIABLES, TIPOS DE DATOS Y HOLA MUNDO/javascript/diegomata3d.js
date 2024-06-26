317590a253fef84264441d7ca81771f182ea67ae
DiegoMata3D <162609962+DiegoMata3D@users.noreply.github.com>
2024-06-25 22:05:52 -0600

    #00 - JavaScript

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/diegomata3d.js
@@ -0,0 +1,28 @@
+//Comentarios
+
+// https://developer.mozilla.org/es/docs/Web/JavaScript
+
+// Comentario en una linea
+
+/* 
+  Esto es un comentario 
+  escrito y compartido 
+  en varias lineas. 
+*/
+
+// Variables y Constantes
+var nombre = "Diego"; //Usar "" convierte a una variable en una cadena de texto
+let edad = 27;
+const despedida = "adios";
+
+// Variables representando los datos primitivos
+var number = 1997;
+var string = "Hola a todos";
+var boolean = true;
+var array = ["verde, azul, rojo"];
+var objeto = {nombre: "Diego", edad: 27};
+var nulo = null;
+var indefinido = undefined;
+
+// Imprimir por terminal 
+console.log('¡Hola, Javascripteros!');
